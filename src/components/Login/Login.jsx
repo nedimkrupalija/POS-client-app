ded97f8ea790dc446fcde4c18c0850c76aedd679
tselimovic2 <115942763+tselimovic2@users.noreply.github.com>
2024-03-23 14:54:36 +0100

    Small changes in login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -8,7 +8,7 @@ const Login = () => {
     return(
         <div className="container">
             <div className="header">
-                <div className="text">Log in</div>
+                <div className="text">Login</div>
                 <div className="underline"></div>
             </div>
              <div className="inputs">
