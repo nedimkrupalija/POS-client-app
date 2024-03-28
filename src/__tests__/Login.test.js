6605ae749f75e445494f81ebcbfbd98a0bf283cc
nedimkrupalija <nkrupalija1@etf.unsa.ba>
2024-03-28 16:30:13 +0100

    change tests to work for fetch

--- src/__tests__/Login.test.js
+++ src/__tests__/Login.test.js
@@ -4,98 +4,98 @@ import axios from 'axios';
 import Login from '../components/Login/Login';
 import '@testing-library/jest-dom'
 
-jest.mock('axios');
 
+global.fetch = jest.fn();
 
 
 describe('Login Component', () => {
   beforeEach(() => {
-    jest.clearAllMocks();
+    fetch.mockClear();
   });
 
   test('renders login form', () => {
     render(<Login />);
     const logins = screen.getAllByText('Login');
     expect(logins[0]).toBeInTheDocument();
-    expect(screen.getByPlaceholderText('Email')).toBeInTheDocument();
-    expect(screen.getByPlaceholderText('Password')).toBeInTheDocument();
+    expect(screen.getByPlaceholderText('Username')).toBeInTheDocument();
+    expect(screen.getByPlaceholderText('PIN')).toBeInTheDocument();
     
   });
 
-  test('handles successful login', async () => {
-    const token = 'mocked-token';
-    axios.post.mockResolvedValueOnce({ data: {"message":"User not found"} });
+  
+  test('redirects to home on successful login', async () => {
     
+    fetch.mockResolvedValueOnce({
+      ok: true,
+      json: async () => ({ token: 'mocked_token' }),
+    });
+
     render(<Login />);
+
     
-    fireEvent.change(screen.getByPlaceholderText('Username'), {
-      target: { value: 'test' },
-    });
-    fireEvent.change(screen.getByPlaceholderText('Pin'), {
-      target: { value: 'test' },
-    });
-    const logins = screen.getAllByText('Login');
-    fireEvent.click(logins[1]);
-    
+    fireEvent.change(screen.getByPlaceholderText('Username'), { target: { value: 'testuser' } });
+    fireEvent.change(screen.getByPlaceholderText('PIN'), { target: { value: '123456' } });
+    fireEvent.click(screen.getByText('Log in'));
+
+  
     await waitFor(() => {
-       expect(axios.post).toHaveBeenCalledTimes(1);
-      
-      expect(axios.post).toHaveBeenCalledWith(
-        'https://pos-app-backend-tim56.onrender.com/auth/login',
-        {
-          username: 'test',
-          password: 'test',
-          role: 'user',
-        },
-        { headers: { 'Content-Type': 'application/json' } }
-      );
-      
+      expect(fetch).toHaveBeenCalledTimes(1);
+      expect(screen.getByText('Manager app')).toBeInTheDocument();
       
     });
-    expect(screen.queryByText('User not found')).not.toBeInTheDocument();
-      expect(screen.queryByText('Invalid password')).not.toBeInTheDocument();
-      expect(screen.queryByText('Login')).not.toBeInTheDocument();
+
+ 
+   
   });
 
-  test('handles failed login', async () => {
-    axios.post.mockRejectedValueOnce(new Error('Invalid credentials'));
-    
+
+  test('displays error message on unsuccessful login', async () => {
+   
+    fetch.mockResolvedValueOnce({
+      ok: false,
+      status: 400,
+      json: async () => ({ message: 'User not found' }),
+    });
+
     render(<Login />);
-    
-    fireEvent.change(screen.getByPlaceholderText('Username'), {
-      target: { value: 'test@example.com' },
-    });
-    fireEvent.change(screen.getByPlaceholderText('Pin'), {
-      target: { value: 'password' },
-    });
-    let logins = screen.getAllByText('Login');
-    fireEvent.click(logins[1]);
-    
+
+  
+    fireEvent.change(screen.getByPlaceholderText('Username'), { target: { value: 'testuser' } });
+    fireEvent.change(screen.getByPlaceholderText('PIN'), { target: { value: '123456' } });
+    fireEvent.click(screen.getByText('Log in'));
+
+  
     await waitFor(() => {
-      expect(axios.post).toHaveBeenCalledTimes(1);
-      expect(axios.post).toHaveBeenCalledWith(
-        'https://pos-app-backend-tim56.onrender.com/auth/login',
-        {
-          username: 'test@example.com',
-          password: 'password',
-          role: 'user',
-        },
-        { headers: { 'Content-Type': 'application/json' } }
-      );
-      expect(screen.queryByText('User not found')).not.toBeInTheDocument();
-      expect(screen.getByText('Invalid password')).toBeInTheDocument();
-      const logins = screen.getAllByText('Login');
-      expect(logins[1]).toBeInTheDocument();
+      expect(fetch).toHaveBeenCalledTimes(1);
+      expect(screen.getByText('User not found')).toBeInTheDocument();
     });
-  });
 
-  test("empty email and password",() => {
+ 
+    
+  });
+  test('displays error message for empty email and password', async () => {
+   
+
+  
+    
+
+    fetch.mockResolvedValueOnce({
+      ok: false,
+      status: 400,
+      json: async () => ({ message: 'User not found' }),
+    });
+
     render(<Login />);
-    let logins = screen.getAllByText('Login');
-    fireEvent.click(logins[1]);
-    expect(screen.getByText('User not found')).toBeInTheDocument();
-    expect(screen.queryByText('Invalid password')).not.toBeInTheDocument();
-    expect(screen.getAllByText('Login')[1]).toBeInTheDocument();
+    fireEvent.change(screen.getByPlaceholderText('Username'), { target: { value: '' } });
+    fireEvent.change(screen.getByPlaceholderText('PIN'), { target: { value: '' } });
+    fireEvent.click(screen.getByText('Log in'));
+    await waitFor(() => {
+      expect(fetch).toHaveBeenCalledTimes(1);
+      expect(screen.getByText('User not found')).toBeInTheDocument();
+      
+    });
+
+  
   });
 
 });
