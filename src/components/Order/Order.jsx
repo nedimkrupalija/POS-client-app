e76fce595c6c174fda8e0ad91810cd9cb47a768d
tselimovic2 <115942763+tselimovic2@users.noreply.github.com>
2024-04-07 23:03:12 +0200

    Added create order table

--- src/components/Order/Order.jsx
+++ src/components/Order/Order.jsx
@@ -3,6 +3,7 @@ import './Order.css';
 import delete_icon from '../../assets/delete.png';
 import edit_icon from '../../assets/edit.png';
 import more_icon from '../../assets/more.png';
+import plus_icon from '../../assets/plus.png';
 
 const Order = () => {
     
@@ -12,48 +13,132 @@ const Order = () => {
             <h2 className='tables-title'>ORDERS</h2>
             <div className='buttons-container'>
                 <button className='buttons'>LIST ORDERS</button>
-                <button className='buttons'>CREATE NEW ORDER</button>
+                <button className='buttons1'>CREATE NEW ORDER</button>
             </div>
-        <div className='table'>
-            <table border="1">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>VAT price</th>
-                        <th>Total Price (Excluding VAT)</th>
-                        <th>Total Price (Including VAT)</th>
-                        <th>Location ID</th>
-                        <th>Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>1</td>
-                        <td>2</td>
-                        <td>3</td>
-                        <td>15</td>
-                        <td>
-                            <img src={edit_icon} alt="Edit" className='edit_icon' />
-                            <img src={delete_icon} alt="Delete" className='delete_icon' />
-                            <img src={more_icon} alt="More" className='more_icon' />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>0.5</td>
-                        <td>2</td>
-                        <td>2.5</td>
-                        <td>7</td>
-                        <td>
-                            <img src={edit_icon} alt="Edit" className='edit_icon' />
-                            <img src={delete_icon} alt="Delete" className='delete_icon' />
-                            <img src={more_icon} alt="More" className='more_icon' />
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+            <div className='table'>
+                <table border="1">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>VAT price</th>
+                            <th>Total Price (Excluding VAT)</th>
+                            <th>Total Price (Including VAT)</th>
+                            <th>Location ID</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>1</td>
+                            <td>2</td>
+                            <td>3</td>
+                            <td>15</td>
+                            <td>
+                                <img src={edit_icon} alt="Edit" className='edit_icon' />
+                                <img src={delete_icon} alt="Delete" className='delete_icon' />
+                                <img src={more_icon} alt="More" className='more_icon' />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>0.5</td>
+                            <td>2</td>
+                            <td>2.5</td>
+                            <td>7</td>
+                            <td>
+                                <img src={edit_icon} alt="Edit" className='edit_icon' />
+                                <img src={delete_icon} alt="Delete" className='delete_icon' />
+                                <img src={more_icon} alt="More" className='more_icon' />
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div className='table'>
+                <table border="1">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Price (Excluding VAT) ($)</th>
+                            <th>Price (Including VAT) ($)</th>
+                            <th>Measurement</th>
+                            <th>BAR-code</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Voda Voda</td>
+                            <td>0.5</td>
+                            <td>1</td>
+                            <td>30</td>
+                            <th>05224678</th>
+                            <td>
+                                <img src={edit_icon} alt="Edit" className='edit_icon' />
+                                <img src={delete_icon} alt="Delete" className='delete_icon' />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>Coca Cola</td>
+                            <td>1</td>
+                            <td>2</td>
+                            <td>50</td>
+                            <td>123456789</td>
+                            <td>
+                                <img src={edit_icon} alt="Edit" className='edit_icon' />
+                                <img src={delete_icon} alt="Delete" className='delete_icon' />
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
+
+
+        <div className='create-order'>
+        <div className='table'>
+                <table border="1">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Price (Excluding VAT) ($)</th>
+                            <th>Price (Including VAT) ($)</th>
+                            <th>Measurement</th>
+                            <th>BAR-code</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Voda Voda</td>
+                            <td>0.5</td>
+                            <td>1</td>
+                            <td>30</td>
+                            <th>05224678</th>
+                            <td>
+                                <img src={plus_icon} alt="Plus" className='plus_icon' />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>Coca Cola</td>
+                            <td>1</td>
+                            <td>2</td>
+                            <td>50</td>
+                            <td>123456789</td>
+                            <td>
+                                <img src={plus_icon} alt="Plus" className='plus_icon' />
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
 
         </>
