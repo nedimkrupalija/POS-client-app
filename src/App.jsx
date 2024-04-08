0e5a2e5cde12a44a440773aa0f879fada21e97e1
Amina Bajrić <115504860+abajric2@users.noreply.github.com>
2024-04-08 21:44:14 +0200

    Logic for assigning users on tables implemented

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,19 @@ import UserTables from './components/UserTables/UserTables.jsx';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-  /*const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
     const jwtToken = Cookies.get('jwt'); 
     setIsLoggedIn(!!jwtToken); 
-  }, []);*/
+  }, []);
   return (
       /*<div>
         {isLoggedIn ? <Home /> : <Login />}     </div>*/
         <Router>
           <Routes>
-          <Route exact path="/" element={<UserTables />} />
+          <Route exact path="/" element={isLoggedIn ? <Home /> : <Login />} />
+          <Route exact path="/tables" element={isLoggedIn ? <UserTables /> : <Login />} />
           </Routes>
         </Router>
   )
--- src/components/UserTables/UserTables.jsx
+++ src/components/UserTables/UserTables.jsx
@@ -1,40 +1,160 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import Cookies from 'js-cookie';
 import './UserTables.css';
 
 const UserTables = () => {
-    
+    const [assignedTables, setAssignedTables] = useState([]);
+    const [otherTables, setOtherTables] = useState([]);
+    const [selectedTables, setSelectedTables] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const token = () => {
+        return Cookies.get("jwt");
+    }
+
+    useEffect(() => {
+        fetchTables();
+    }, []);
+
+    const fetchTables = async () => {
+        const locationId = localStorage.getItem('locationId');
+        const userId = localStorage.getItem('userId');
+        if (locationId && userId) {
+            const headers = {
+                Authorization: token()
+            };
+            fetchData('GET', `http://localhost:3000/location/${locationId}/tables`, null, headers)
+                .then(response => {
+                    const assigned = response.filter(table => table.UserId === userId);
+                    const others = response.filter(table => table.UserId !== userId);
+                    setAssignedTables(assigned);
+                    setOtherTables(others);
+                })
+                .catch(error => {
+                    console.error('Error fetching tables:', error);
+                });
+        }
+    }
+
+    const fetchData = async (method, url, requestData = null, headers = {}) => {
+        try {
+            const options = {
+                method: method,
+                headers: {
+                    ...headers,
+                    'Content-Type': 'application/json'
+                }
+            };
+            if (requestData) {
+                options.body = JSON.stringify(requestData);
+            }
+            const response = await fetch(url, options);
+            const data = await response.json();
+            if (!response.ok) {
+                throw new Error(data.message || 'Error fetching data');
+            }
+            const extendedToken = response.headers.get('Authorization');
+            console.log(extendedToken);
+            if (extendedToken) {
+                Cookies.set('jwt', extendedToken, { expires: 1 / 48 });
+            }
+            return data;
+        } catch (error) {
+            throw new Error(error.message || 'Error fetching data');
+        }
+    };
+
+    const handleCheckboxChange = (tableId) => {
+        setSelectedTables(prevSelectedTables => {
+            if (prevSelectedTables.includes(tableId)) {
+                return prevSelectedTables.filter(id => id !== tableId);
+            } else {
+                return [...prevSelectedTables, tableId];
+            }
+        });
+    }
+
+    const handleAssignYourself = () => {
+        setIsLoading(true);
+        const headers = {
+            Authorization: token()
+        };
+        const requestBody = {
+            tables: selectedTables
+        };
+        fetchData('POST', 'http://localhost:3000/user/tables', requestBody, headers)
+            .then(() => {
+                setIsLoading(false);
+                setSelectedTables([]);
+                fetchTables();
+            })
+            .catch(error => {
+                console.error('Error assigning tables:', error);
+                setIsLoading(false);
+            });
+    }
+
     return (
         <>
-        <div className='tables-to-assign'>
-            <h2 className='tables-title'>TABLES TO ASSIGN</h2>
-        <div className='table'>
-            <table border="1">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Name</th>
-                        <th>User ID</th>
-                        <th>Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>Tajra</td>
-                        <td>27</td>
-                        <td><button className='buttons'>ASSIGN</button></td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>Amina</td>
-                        <td>25</td>
-                        <td><button className='buttons'>ASSIGN</button></td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-        </div>
+            <div className='tables-to-assign'>
+                <h2 className='tables-title'>TABLES TO ASSIGN</h2>
+                <button className='active-buttons assign-yourself-button' onClick={handleAssignYourself} disabled={selectedTables.length === 0 || isLoading}>Assign Yourself</button>
+                <div className='table'>
+                    <table border="1">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Name</th>
+                                <th>User ID</th>
+                                <th>Assign</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {otherTables.map(table => (
+                                <tr key={table.id}>
+                                    <td>{table.id}</td>
+                                    <td>{table.name}</td>
+                                    <td>{table.UserId ? table.UserId : 'NOT ASSIGNED'}</td>
+                                    <td>
+                                        {
+                                            table.UserId === localStorage.getItem('userId') ?
+                                                <input
+                                                    type="checkbox"
+                                                    className='tables-check-box'
+                                                    checked={selectedTables.includes(table.id)}
+                                                    onChange={() => handleCheckboxChange(table.id)}
+                                                /> :
+                                                'ASSIGNED'
+                                        }
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
 
+            <div className='tables-to-assign'>
+                <h2 className='tables-title'>TABLES ASSIGNED TO YOU</h2>
+                <div className='table'>
+                    <table border="1">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Name</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {assignedTables.map(table => (
+                                <tr key={table.id}>
+                                    <td>{table.id}</td>
+                                    <td>{table.name}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </>
     );
 };
